93ad694e7a2d777bd86e8c5ec31a94a40752c6bc
5manojrawat5 <5mercy5mercy5@gmail.com>
2023-05-11 21:50:26 +0530

    final commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,35 @@
+import './topbar.scss'
+import {Email,Person} from '@mui/icons-material'
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={'topbar ' + (menuOpen && "active")}>
+
+        <div className="wrapper">
+
+        <div className="left">
+          <a href="#contact" className='logo'>anyone.</a>
+          <div className="itemContainer">
+          <Person className="icon" />
+          <span>+91 9084487403</span>
+          </div>
+          <div className="itemContainer">
+          <Email className='icon' />
+          <span>dummy@gmail.com</span>
+          </div>
+
+        </div>
+
+        <div className="right">
+        <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}  >
+          <span className='line1'></span>
+          <span className='line2'></span>
+          <span className='line3'></span>
+        </div>
+        </div>
+
+        </div>
+
+    </div>
+  )
+}
